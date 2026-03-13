ded3ef6161953fbe055491248f0cf0f37f7dfe1d
R KISHORE <vindhiyavidhya@gmail.com>
2026-03-14 01:32:20 +0530

    UI: Fix 'Free' aspect ratio selection in ImageCropper

--- src/ImageCropper.jsx
+++ src/ImageCropper.jsx
@@ -25,7 +25,7 @@ export function ImageCropper({ image, onCropComplete, onCancel, dark }) {
     };
 
     const RATIOS = [
-        { label: "Free", val: null },
+        { label: "Free", val: undefined },
         { label: "1:1", val: 1 },
         { label: "4:3", val: 4 / 3 },
         { label: "16:9", val: 16 / 9 },
@@ -43,6 +43,7 @@ export function ImageCropper({ image, onCropComplete, onCancel, dark }) {
                 background: dark ? '#1a1a2e' : '#fff', borderRadius: 20, overflow: 'hidden'
             }}>
                 <Cropper
+                    key={aspect}
                     image={image}
                     crop={crop}
                     zoom={zoom}
@@ -50,6 +51,7 @@ export function ImageCropper({ image, onCropComplete, onCancel, dark }) {
                     onCropChange={onCropChange}
                     onCropComplete={onCropCompleteInternal}
                     onZoomChange={onZoomChange}
+                    showGrid={true}
                 />
             </div>
 
